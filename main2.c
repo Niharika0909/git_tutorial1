43b10f88ae43d915b4e7b96a027fb38b7e8613f0
pranav <pranav.gupta0909@gmail.com>
2020-08-10 20:55:33 +0530

    Update On 10-8-2020   |   Create 3 files | modify 2 files | delete student.exe

--- main2.c
+++ main2.c
@@ -1,11 +1,12 @@
 #include <stdio.h>   // Here we use Snippets "start" 
 #include <conio.h>
+#include <string.h>
 
 
 
 int main(){
      printf("XXX \n");
-     int *a;
+     /*int *a;
      int b =98;
      a = &b;
      printf("Value of 'B' %d \n",b);
@@ -17,6 +18,55 @@ int main(){
        printf("Value of A %d \n",*(&a));   //Doubt  (Clear)   
      //printf("Value of 'A': %d",*a);
 
+     */
+      /*
+     int a[4];
+     int i;
+     int *b;
+     b = &a[0];
+    for(i=0;i<4;i++){
+      printf("Enter the value of Array %d \n",i+1);
+      scanf("%d",b);
+      b++;
+    }
+    for(i=0;i<4;i++){
+      printf("Elemrnt : %d is%d \n",i+1,a[i]);
+    }
+     
+    */
+
+    /*
+     char v[] = {'a','d','d','e','\0'};
+     char c[] = "pranav \n";
+     printf("%s",c);   //%s is a format specifier  
+
+    char name[50];
+    printf("Enter your fuckink name: \n");
+    //scanf("%s \n",&name);
+    //printf("%s",name);
+
+    gets(name);
+    printf("\n");
+    puts(name);
+    */
+
+    char *h = "Pranav";
+    puts(h);
+    int len = strlen(h);
+    printf("Length %d \n",len);
+
+                                                   //when we use char *ptr = "pranav"  ---- they can also reinitialize
+                                                  // when we use char ptr[] = "pranav"  ---- they can't initialize
+
+    
+
+
+    // check strcmp()
+
+    
+
+
+    return 0;
 
 
      
---
+++ main3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <conio.h>
+
+
+struct student{
+    int a ;
+    int g ;
+
+};
+int main(){
+
+    struct student s;
+    s.a = 45;
+    s.g = 180101221;
+    printf("%d %d \n",s.a,s.g);
+
+    return 0;
+}
